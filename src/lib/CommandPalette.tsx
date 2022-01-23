15cbd008db755672f62a3bd5641fd8ff1ae31762
Aditya Agarwal <adityaa803@gmail.com>
2022-01-23 14:38:37 +0530

    command palette box

--- src/lib/CommandPalette.tsx
+++ src/lib/CommandPalette.tsx
@@ -1,15 +1,37 @@
-import { Component, For, Show } from 'solid-js';
+import { Component, For, onMount, Show } from 'solid-js';
+import tinykeys from 'tinykeys';
 import { useStore } from './StoreContext';
 import { CommandPalettePortal } from './CommandPalettePortal';
 import styles from './CommandPalette.module.css';
 
-export const CommandPalette: Component = () => {
-  const [state] = useStore();
+export const CommandPaletteInternal: Component = () => {
+  const [state, { close }] = useStore();
+  let wrapperRef;
+
+  function handleWrapperClick() {
+    close();
+  }
+
+  function handlePaletteClick(event: MouseEvent) {
+    event.stopPropagation();
+  }
+
+  onMount(() => {
+    tinykeys(wrapperRef, {
+      Escape: () => {
+        console.log('escape press');
+        close();
+      },
+    });
+  });
 
   return (
-    <CommandPalettePortal>
-      <Show when={state.visibility === 'opened'}>
-        <div class={styles.wrapper}>
+    <div class={styles.wrapper} ref={wrapperRef} onClick={handleWrapperClick}>
+      <div class={styles.palette} onClick={handlePaletteClick}>
+        <div>
+          <input type="text" placeholder="Search for stuff" />
+        </div>
+        <div>
           <ul>
             <For each={Object.values(state.actions)} fallback={<div>No Actions</div>}>
               {(action) => {
@@ -28,6 +50,18 @@ export const CommandPalette: Component = () => {
             </For>
           </ul>
         </div>
+      </div>
+    </div>
+  );
+};
+
+export const CommandPalette: Component = () => {
+  const [state] = useStore();
+
+  return (
+    <CommandPalettePortal>
+      <Show when={state.visibility === 'opened'}>
+        <CommandPaletteInternal />
       </Show>
     </CommandPalettePortal>
   );
