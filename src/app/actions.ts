cc13f2eb17e7cad56ca19e968bd580eb25b76b65
Aditya Agarwal <adityaa803@gmail.com>
2022-01-23 20:42:49 +0530

    add more test actions

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -23,7 +23,46 @@ const secondAction = defineAction({
   },
 });
 
+const thirdAction = defineAction({
+  id: 'third',
+  title: 'Third Action',
+  run: () => {
+    console.log('run third');
+  },
+});
+
+const fourthAction = defineAction({
+  id: 'fourth',
+  title: 'Fourth Action',
+  subtitle: 'Do something random with this action',
+  run: () => {
+    console.log('run fourth');
+  },
+});
+
+const fifthAction = defineAction({
+  id: 'fifth',
+  title: 'Fifth Action',
+  subtitle: 'You will get another console log by clicking this',
+  shortcut: 'l',
+  run: () => {
+    console.log('run fifth');
+  },
+});
+
+const sixthAction = defineAction({
+  id: 'sixth',
+  title: 'Sixth Action',
+  run: () => {
+    console.log('run sixth');
+  },
+});
+
 export const actions = {
   [firstAction.id]: firstAction,
   [secondAction.id]: secondAction,
+  [thirdAction.id]: thirdAction,
+  [fourthAction.id]: fourthAction,
+  [fifthAction.id]: fifthAction,
+  [sixthAction.id]: sixthAction,
 };
