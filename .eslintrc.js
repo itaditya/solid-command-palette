ae64d926dc0f95493cd5a411e9f5a770d26759e8
Aditya Agarwal <adityaa803@gmail.com>
2022-01-29 17:30:01 +0530

    add linting with tsc & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "@typescript-eslint/no-unused-vars": ["error", {
+            varsIgnorePattern: "^_",
+            argsIgnorePattern: "^_",
+        }],
+    }
+}
