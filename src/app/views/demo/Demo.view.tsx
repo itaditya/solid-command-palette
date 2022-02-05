f47aeb80e43d42d430abed2ed6d0b1f32afe8870
Aditya Agarwal <adityaa803@gmail.com>
2022-02-05 17:56:01 +0530

    improve UI for first 2 actions

--- src/app/views/demo/Demo.view.tsx
+++ src/app/views/demo/Demo.view.tsx
@@ -1,12 +1,15 @@
-import { Component, createSignal } from 'solid-js';
+import { Component, createSignal, Show } from 'solid-js';
 import { Root, CommandPalette, KbdShortcut } from '../../../lib';
 import { actions } from './actions';
 import { DynamicActionContextDemo } from './DynamicActionContextDemo/DynamicActionContextDemo';
+import utilStyles from '../../utils.module.css';
 import styles from './Demo.module.css';
+
+type Profile = 'personal' | 'work';
 
 const DemoView: Component = () => {
   const [count, setCount] = createSignal(0);
-  const [profile, setProfile] = createSignal('personal');
+  const [profile, setProfile] = createSignal<Profile>('personal');
 
   const increment = () => {
     setCount((prev) => (prev += 1));
@@ -25,7 +28,7 @@ const DemoView: Component = () => {
   const handleProfileChange = (event: Event) => {
     const targetElem = event.currentTarget as HTMLSelectElement;
 
-    const newProfile = targetElem.value;
+    const newProfile = targetElem.value as Profile;
     setProfile(newProfile);
   };
 
@@ -37,7 +40,7 @@ const DemoView: Component = () => {
 
   return (
     <Root actions={actions} actionsContext={actionsContext}>
-      <div>
+      <div class={styles.demoWrapper}>
         <CommandPalette />
         <section class={styles.introSection}>
           <h1 class={styles.introHeading}>
@@ -45,23 +48,62 @@ const DemoView: Component = () => {
             <KbdShortcut class={styles.introShortcutKey} shortcut="$mod+k" />
           </h1>
         </section>
-        <section>
-          <h3>Trigger First Action increases the count.</h3>
-          <p>
-            Count is <strong>{count()}</strong>
-          </p>
-          <button onClick={increment}>Increase Count</button>
+        <section class={styles.demoSection}>
+          <div>
+            <h3 class={utilStyles.stripSpace}>Controlling application state</h3>
+            <p>
+              We have a <strong>count</strong> signal and an <strong>increment</strong> function to
+              increase its value by 1.
+            </p>
+            <p>You can trigger it by clicking on the button below it.</p>
+            <p>
+              We have also bound this increment function to our first action and a keyboard shortcut
+            </p>
+          </div>
+          <div class={styles.demoInteraction}>
+            <strong class={styles.countValue}>{count()}</strong>
+            <button class={styles.demoBtn} onClick={increment}>
+              Increment count by 1
+            </button>
+            <p class={styles.demoInteractionDesc}>
+              Try holding <KbdShortcut shortcut="$mod+e" />
+            </p>
+          </div>
         </section>
-        <section>
-          <h3>Trigger Third Action to toggle profile</h3>
-          <p>
-            Active profile is <strong>{profile()}</strong>
-          </p>
-          <select value={profile()} onChange={handleProfileChange}>
-            <option value="personal">Personal</option>
-            <option value="work">Work</option>
-          </select>
-          <p>**Fourth Action will only appear in Work profile</p>
+        <section class={styles.demoSection}>
+          <div>
+            <h3 class={utilStyles.stripSpace}>Conditionally enable actions</h3>
+            <p>
+              We have a <strong>profile</strong> signal and a <strong>toggleProfile</strong>{' '}
+              function to toggle between <strong>personal</strong> & <strong>work</strong>.
+            </p>
+            <p>You can trigger it by selecting options in the menu.</p>
+            <p>
+              The <strong>Join the Standup Meeting</strong> will only appear in Work profile.
+            </p>
+          </div>
+          <div class={styles.demoInteraction}>
+            <p class={styles.demoInteractionDesc}>
+              Active profile is <strong>{profile()}</strong>
+            </p>
+            <select
+              aria-label="Select Profile"
+              class={styles.profileMenu}
+              value={profile()}
+              onChange={handleProfileChange}
+            >
+              <option value="personal">Personal</option>
+              <option value="work">Work</option>
+            </select>
+            <p class={styles.demoInteractionDesc}>
+              Try pressing <KbdShortcut shortcut="$mod+Shift+p" />
+            </p>
+            <Show when={profile() === 'work'}>
+              <p class={styles.demoInteractionDesc}>
+                Try pressing <KbdShortcut shortcut="$mod+j" />
+              </p>
+            </Show>
+          </div>
         </section>
         <DynamicActionContextDemo />
       </div>
