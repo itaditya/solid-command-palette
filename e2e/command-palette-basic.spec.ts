9c606dd25f2b68b2eef48883ab7e8b2747a932e3
Aditya Agarwal <adityaa803@gmail.com>
2022-02-14 01:49:54 +0530

    basic e2e test

--- e2e/command-palette-basic.spec.ts
+++ e2e/command-palette-basic.spec.ts
@@ -1,8 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Test basic interactions of Command Palette', () => {
-  test('should be able to open command palette & find actions by title', async ({ page }) => {
+  test('should be able to open command palette & run first action', async ({ page }) => {
     await page.goto('/demo');
-    await expect(page.locator('text=Bring it up by pressing')).toBeVisible();
+
+    await page.keyboard.press('Meta+k'); // on Mac
+    await page.keyboard.press('Control+k'); // on Linux
+
+    await page.locator('text=Increment Counter by 1').click();
+
+    await expect(page.locator('strong:has-text("1")')).toBeVisible();
   });
 });
