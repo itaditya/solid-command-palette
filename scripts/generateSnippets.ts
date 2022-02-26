732e7a038b0522d1f60fa713753ad7c909084def
Aditya Agarwal <adityaa803@gmail.com>
2022-02-26 12:28:06 +0530

    switch to ts-node script

--- scripts/generateSnippets.ts
+++ scripts/generateSnippets.ts
@@ -1,11 +1,14 @@
-const path = require('path');
-const fs = require('fs/promises');
-const shiki = require('shiki');
+import path from 'path';
+import fs from 'fs/promises';
+import { getHighlighter, Highlighter } from 'shiki';
+
+type SnippetId = string;
+type SnippetIdList = Array<SnippetId>;
 
 async function getSnippetIdList() {
   const snippetFolderPath = path.join(process.cwd(), 'snippets');
   const snippetFilePaths = await fs.readdir(snippetFolderPath);
-  const snippetIdList = [];
+  const snippetIdList: SnippetIdList = [];
 
   snippetFilePaths.forEach((file) => {
     const fileName = path.basename(file);
@@ -20,18 +23,18 @@ async function getSnippetIdList() {
   return snippetIdList;
 }
 
-async function readPlainSnippet(snippetId) {
+async function readPlainSnippet(snippetId: SnippetId) {
   const snippetFilePath = path.join(process.cwd(), 'snippets', `${snippetId}.snippet.tsx`);
   const plainSnippet = await fs.readFile(snippetFilePath, 'utf8');
   return plainSnippet;
 }
 
-async function writeHighlightedSnippet(snippetId, highlightedSnippet) {
+async function writeHighlightedSnippet(snippetId: SnippetId, highlightedSnippet: string) {
   const snippetFilePath = path.join(process.cwd(), 'public', 'snippets', `${snippetId}.html`);
   await fs.writeFile(snippetFilePath, highlightedSnippet, 'utf8');
 }
 
-async function generateSnippet(highlighter, snippetId) {
+async function generateSnippet(highlighter: Highlighter, snippetId: SnippetId) {
   const plainSnippet = await readPlainSnippet(snippetId);
   const highlightedSnippet = highlighter.codeToHtml(plainSnippet, {
     lang: 'tsx',
@@ -40,7 +43,7 @@ async function generateSnippet(highlighter, snippetId) {
 }
 
 async function generateSnippets() {
-  const highlighter = await shiki.getHighlighter({
+  const highlighter = await getHighlighter({
     theme: 'github-dark',
     langs: ['tsx'],
   });
