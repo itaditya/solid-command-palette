2950f91b4e12eeee9eba53236f18de5adc5d82ea
Silson sapkota <silson007@gmail.com>
2023-04-09 22:13:31 +0545

    Fix typo in statement 'Make your tool lightning fast'

--- src/app/views/home/Home.view.tsx
+++ src/app/views/home/Home.view.tsx
@@ -11,7 +11,9 @@ const HomeView: Component = () => {
     <section class={styles.wrapper}>
       <main class={styles.main}>
         <h3 class={`${styles.title} ${utilStyles.stripSpace}`}>
-          <span>Make your tool lighting fast</span>
+          <span>
+            Make your tool lightning fast
+          </span>
           <span class={styles.titleSecondary}>
             <span>with </span>
             <button
